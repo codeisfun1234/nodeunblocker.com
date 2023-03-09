4b3c2e5c826cfc7ee1c30cd00ef444a07a2d24dc
coder0107git <76132013+coder0107git@users.noreply.github.com>
2023-03-09 14:12:44 -0700

    Update app.js
    Made quotes consistent (`"` instead of `'`).  Transitioned from `var` and anonymous functions to arrow functions, `const`, and `let`. Made the `/no-js` path actually work. Unblocked Google (didn't realize that it matched any of Google's subdomains).

--- app.js
+++ app.js
@@ -9,47 +9,49 @@
  * Released under the terms of the Affero GPL v3
  */
 
-var url = require('url');
-var querystring = require('querystring');
-var express = require('express');
-var Unblocker = require('unblocker');
-var Transform = require('stream').Transform;
-var youtube = require('unblocker/examples/youtube/youtube.js')
-var blacklist = require("unblocker/examples/blacklist/blacklist.js");
+const url = require("url");
+const querystring = require("querystring");
+const express = require("express");
+const Unblocker = require("unblocker");
+const Transform = require("stream").Transform;
+const youtube = require("unblocker/examples/youtube/youtube.js");
+const blacklist = require("unblocker/examples/blacklist/blacklist.js");
 
-var app = express();
+const app = express();
 
-var unblocker = new Unblocker({
-    prefix: '/view/',
+const unblockerConfig = {
+    prefix: "/view/",
     requestMiddleware: [
         youtube.processRequest,
-        blacklist({
+        /*blacklist({
             blockedDomains: ["google.com"],
             message: "The requested URL doesn't work on this site.",
-        }),
+        }),*/
         blacklist({
-            blockedDomains: ["example.com"],
+            blockedDomains: ["evil-website"],
             message: "The requested url is not permitted (has been blocked).",
         }),
     ],
-});
+};
+
+const unblocker = new Unblocker(unblockerConfig);
 
 // this line must appear before any express.static calls (or anything else that sends responses)
 app.use(unblocker);
 
 // serve up static files *after* the proxy is run
-app.use('/', express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + "/public"));
 
 // this is for users who's form actually submitted due to JS being disabled or whatever
-app.get("/no-js", function(req, res) {
+app.get("/no-js", (req, res) => {
     // grab the "url" parameter from the querystring
-    var site = querystring.parse(url.parse(req.url).query).url;
-    // and redirect the user to /proxy/url
+    const site = querystring.parse(url.parse(req.url).query).url;
+    
+    // and redirect the user to /view/url
     res.redirect(unblockerConfig.prefix + site);
 });
 
 const port = process.env.PORT || process.env.VCAP_APP_PORT || 8080;
 
-app.listen(port, function() {
-    console.log(`node unblocker process listening at http://localhost:${port}/`);
-}).on("upgrade", unblocker.onUpgrade); // onUpgrade handles websockets
+app.listen(port, () => console.log(`node unblocker process listening at http://localhost:${port}/`))
+   .on("upgrade", unblocker.onUpgrade); // onUpgrade handles websockets
